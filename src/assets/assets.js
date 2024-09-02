cb2a193031d3428669d8d72ede2496988144c810
adityagupta128 <aditya.gupta1-coend@bvp.edu.in>
2024-09-02 22:48:03 +0530

    ORION

---
+++ src/assets/assets.js
@@ -0,0 +1,34 @@
+import bulb_icon from './bulb-icon.png'
+import menu_icon from './menu_icon.png'
+import plus_icon from './plus_icon.png'
+import history_icon from './history_icon.png'
+import message_icon from './message_icon.png'
+import settings_icon from './settings_icon.png'
+import question_icon from './question_icon.png'
+import user_icon from './user_icon.png'
+import compass_icon from './compass_icon.png'
+import code_icon from './code_icon.png'
+import team_icon from './team_icon.png'
+import gallery_icon from './gallery_icon.png'
+import mic_icon from './mic_icon.png'
+import send_icon from './send_icon.png'
+import ai_icon from './ai_icon.png'
+
+
+export const assets = {
+    bulb_icon,
+    menu_icon,
+    plus_icon,
+    history_icon,
+    message_icon,
+    settings_icon,
+    question_icon,
+    user_icon,
+    compass_icon,
+    code_icon,
+    team_icon,
+    gallery_icon,
+    mic_icon,
+    send_icon,
+    ai_icon    
+}
